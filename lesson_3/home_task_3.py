0b235de86f6ee24f4d8c2186b9849ba1bf0e58b9
Rayy Dubb <rayydubb808@gmail.com>
2021-11-25 17:19:28 +0200

    upd 3

--- lesson_3/home_task_3.py
+++ lesson_3/home_task_3.py
@@ -1,14 +1,6 @@
 year = int(input("Введите год: "))
 
-
-def leap() -> object:
-    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-        print("Год невысокосный")
-    else:
-        print("Год высокосный")
-
-
 if 1900 < year < 1000000:
-    leap()
+    print("Год не высокосный" if year % 4 == 0 and year % 100 != 0 or year % 400 == 0 else "Год высокосный")
 else:
     print("Год не соответсвует условию")

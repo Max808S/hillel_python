ec5768d684c3e87a1837f36b5bd83400bb40a271
Rayy Dubb <rayydubb808@gmail.com>
2021-11-25 17:30:13 +0200

    upd 4

--- lesson_3/home_task_3.py
+++ lesson_3/home_task_3.py
@@ -1,6 +1,6 @@
 year = int(input("Введите год: "))
 
 if 1900 < year < 1000000:
-    print("Год не высокосный" if year % 4 == 0 and year % 100 != 0 or year % 400 == 0 else "Год высокосный")
+    print("Год высокосный" if year % 4 == 0 and year % 100 != 0 or year % 400 == 0 else "Год не высокосный")
 else:
     print("Год не соответсвует условию")

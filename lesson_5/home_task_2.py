4036096e09f69fbdad5dd8670c24514c96312962
Rayy Dubb <rayydubb808@gmail.com>
2021-12-05 18:26:53 +0200

    home_task_2.py upd 1

---
+++ lesson_5/home_task_2.py
@@ -0,0 +1,26 @@
+import random
+
+comp_number = random.randint(1, 100)
+user_number = -1
+counter = 0
+
+print('Hi :) You need to guess a number from 1 to 100')
+
+while user_number != comp_number:
+    user_number = int(input('Enter any number: '))
+    counter += 1
+    if user_number > 100:
+        print(f'Sorry, your number {user_number} is larger than 100. Try again: ')
+        continue
+    elif user_number < 1:
+        print(f'Sorry, your number {user_number} is less than 1. Try again: ')
+        continue
+    elif user_number > comp_number:
+        print(f'Almost.. your number {user_number} bigger than hidden number. Try again: ')
+        continue
+    elif user_number < comp_number:
+        print(f'Almost.. your number {user_number} lowest than hidden number. Try again: ')
+        continue
+    else:
+        user_number == user_number
+    print(f'Congrats! You guessed the number on {counter} attempts. Winner number {comp_number} :)')

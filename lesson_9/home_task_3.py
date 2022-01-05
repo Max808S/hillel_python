0e353ccb8e1feffefc585230dd3899fd1367eb63
Rayy Dubb <rayydubb808@gmail.com>
2022-01-05 13:57:29 +0200

    home_task_3.py upd 2

--- lesson_9/home_task_3.py
+++ lesson_9/home_task_3.py
@@ -1,17 +1,11 @@
 import random
 
 
-def generator(a=int, b=int):
-    num_list = [random.randint(a, b) for i in range(1, 100)]
-    return num_list
+def generate(value=None):
+    for _ in range(100):
+        yield random.randint(1, 100)
 
 
-user_number_1 = int(input("Write the first number: "))
-user_number_2 = int(input("Write the second number: "))
-
-res = generator(user_number_1, user_number_2)
-# print(f'{res}')
-
-print(f'List of numbers that were created as a result '
-      f'applying a lambda function to each element: \n '
-      f'{" ".join(map(str, res))}')
+if __name__ == '__main__':
+    print(f'List of numbers that were created as a result '
+          f'of the generator function: \n{" ".join(map(str, generate()))}')

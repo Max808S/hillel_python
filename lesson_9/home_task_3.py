aea10d91e070c289e08d61913e733f46df1e741e
Rayy Dubb <rayydubb808@gmail.com>
2022-01-05 17:09:45 +0200

    home_task_3.py upd 3

--- lesson_9/home_task_3.py
+++ lesson_9/home_task_3.py
@@ -1,11 +1,11 @@
 import random
 
 
-def generate(value=None):
+def generate(a, b):
     for _ in range(100):
-        yield random.randint(1, 100)
+        yield random.randint(a, b)
 
 
 if __name__ == '__main__':
-    print(f'List of numbers that were created as a result '
-          f'of the generator function: \n{" ".join(map(str, generate()))}')
+    print(f'List of numbers that were created as a result of the generator function: '
+          f'\n{" ".join(map(str, generate((int(input("First number: "))), (int(input("Second number: "))))))}')
